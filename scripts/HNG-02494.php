d8aea2d1f3945399759191ac811685313e783cb9
Tisighe Livinstone <tisighelivinstone1b@gmail.com>
2020-06-03 21:05:11 +0100

    HNG-02494.php

---
+++ scripts/HNG-02494.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hello World, 
+this is Tisighe Livinstone
+with INTERNSHIP ID: [HNG-02494] 
+using [PHP]";
+?>
--- scripts/hng02199.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Hello World, 
-this is Awe Oluwaseun Yes
-with HNGi7 ID [02199] 
-using [PHP]";
-?>
