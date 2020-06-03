cf7d05fefe87e4235289ed983bd99c960dba2230
Tisighe Livinstone <tisighelivinstone1b@gmail.com>
2020-06-04 00:11:54 +0100

    Updating HNG-02494.js

---
+++ scripts/HNG-02494.js
@@ -0,0 +1,6 @@
+var name = "Tisighe Livinstone Khan";
+var id = "HNG-02494";
+var email = "tisighelivinstone1b@gmail.com";
+var language = "JavaScript";
+
+document.write("Hello World, This is " +name+ " HNGi7 ID " +id+ " using " +language+ " for stage 2 task my " +email+ " email" );
