969508d366fb817a5f0fd78f8caab248dc363df0
Tisighe Livinstone <tisighelivinstone1b@gmail.com>
2020-06-03 22:19:50 +0100

    Updating Script.js

--- scripts/HNG-02494.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Hello World, 
-this is Tisighe Livinstone
-with INTERNSHIP ID: [HNG-02494] 
-using [PHP]";
-?>
---
+++ scripts/script.js
@@ -0,0 +1,1 @@
+console.log("Hello World, this is Tisighe Livinstone Khan with HNGi7 ID [02494] using [JavaScript] for stage 2 task");
