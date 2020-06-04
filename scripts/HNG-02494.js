33d310fd8e97633512c9c7faf73be1c71510fdba
TisigheLivinstone <50095980+TisigheLivinstone@users.noreply.github.com>
2020-06-04 08:16:36 +0100

    Update HNG-02494.js

--- scripts/HNG-02494.js
+++ scripts/HNG-02494.js
@@ -3,4 +3,4 @@ var id = "HNG-02494";
 var email = "tisighelivinstone1b@gmail.com";
 var language = "JavaScript";
 
-document.write("Hello World, This is " +name+ " HNGi7 ID " +id+ " using " +language+ " for stage 2 task my " +email+ " email" );
+document.write("Hello World, This is " +name+ " HNGi7 ID " +id+ " using " +language+ " for stage 2. " +email+);
