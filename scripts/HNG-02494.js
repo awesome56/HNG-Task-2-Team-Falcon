c67e9ef7729d0df15fa02ceba5af841309e0f4e3
TisigheLivinstone <50095980+TisigheLivinstone@users.noreply.github.com>
2020-06-05 08:49:11 +0100

    Update HNG-02494.js

--- scripts/HNG-02494.js
+++ scripts/HNG-02494.js
@@ -2,7 +2,7 @@
 var values = ["Tisighe Livinstone Khan","HNG-02494","JavaScript","tisighelivinstone1b@gmail.com"]
 
 function myfunc() {
-    console.log('Hello world, this is '+values[0]+' with HNGi7 ID '+values[1]+' using '+values[2]+' for stage 2 task. '+values[3]);
+    console.log('Hello World, this is '+values[0]+' with HNGi7 ID '+values[1]+' using '+values[2]+' for stage 2 task. '+values[3]);
 
 }
 myfunc();
